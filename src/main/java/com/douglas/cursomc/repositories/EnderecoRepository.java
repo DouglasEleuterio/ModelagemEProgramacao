f51d565c300367a790abfdbb5d79481654661494
douglas <douglas@douglas-pc>
2019-09-03 02:52:55 -0300

    Criado o Endpoint de Clientes.

---
+++ src/main/java/com/douglas/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,28 @@
+package com.douglas.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.douglas.cursomc.domain.Endereco;
+
+/**
+ * Classe responsável pelas operações no banco de dados.
+ * Foi utilizada a interface JpaRepository, tal interface manipula o acesso aos dados e encapsula as operações.
+ * As classes do pacote Repositories são as "ultimas" classes quando observamos a arquitetura de Camada MVC.
+ * As classes são responsáveis por acessar o banco de dados, realizar as alterações devidas.
+ * @version 0.0.1
+ * @author Douglas
+ * @since 30/07/2019 
+ * @see Categoria 
+ * @see CategoriaService
+ * @see JpaRepository
+ */
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+	/*
+	 * Interface que estende JpaRepository para fornecer acesso aos dados.
+	 * É passado o tipo de Obj. que deseja manipular e o tipo de dado do identificador.
+	 * Nesse caso Integer (Definido no ID da classe de dominio).
+	 * 
+	 */
+	
+}
