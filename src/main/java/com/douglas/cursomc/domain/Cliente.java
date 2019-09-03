643c965e87b1ae88754ebcabf33206c376bc4e31
Douglas Eleuterio <elettelecom@gmail.com>
2019-09-03 03:12:07 -0300

    Correção de Relacionamento
    Não Estava serializando os endereços dos clientes.

--- src/main/java/com/douglas/cursomc/domain/Cliente.java
+++ src/main/java/com/douglas/cursomc/domain/Cliente.java
@@ -36,7 +36,7 @@ public class Cliente implements Serializable{
     private String cpfOuCnpj;
     private Integer tipo;
     
-    @JsonBackReference //O cliente pode serializar o endereço
+    //@JsonBackReference //O cliente pode serializar o endereço
     @OneToMany(mappedBy = "cliente")
     private List<Endereco> enderecos = new ArrayList<>();
     
