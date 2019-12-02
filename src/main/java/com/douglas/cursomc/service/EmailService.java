72c593ee976de528f4f39276d56aab7e5fab4d7f
Douglas Eleuterio <elettelecom@gmail.com>
2019-12-02 14:24:08 -0300

    Reenviando
    Novo Commit

--- src/main/java/com/douglas/cursomc/service/EmailService.java
+++ src/main/java/com/douglas/cursomc/service/EmailService.java
@@ -3,10 +3,14 @@ package com.douglas.cursomc.service;
 import com.douglas.cursomc.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 public interface EmailService {
     void sendOrderConfirmationEmail (Pedido pedido);
 
     void sendEmail(SimpleMailMessage msg);
 
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
 
+    void sendHtmlEmail (MimeMessage msg);
 }
--- src/main/java/com/douglas/cursomc/service/SmtpEmailService.java
+++ src/main/java/com/douglas/cursomc/service/SmtpEmailService.java
@@ -5,11 +5,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import javax.mail.internet.MimeMessage;
 
 public class SmtpEmailService extends AbstractEmailService {
 
     @Autowired
     private MailSender mailSender; // Instancia um objeto com os dados do email no application-dev.properties
+    @Autowired
+    private JavaMailSender javaMailSender;
 
     private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
 
@@ -17,6 +22,13 @@ public class SmtpEmailService extends AbstractEmailService {
     public void sendEmail(SimpleMailMessage msg) {
         LOG.info("Enviando Email...");
         mailSender.send(msg);
+        LOG.info("Email Enviado");
+    }
+
+    @Override
+    public void sendHtmlEmail(MimeMessage msg) {
+        LOG.info("Enviando Email...");
+        javaMailSender.send(msg);
         LOG.info("Email Enviado");
     }
 }
